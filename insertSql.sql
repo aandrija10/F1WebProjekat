4085e8d16bae6f891369ec36af8368201b183792
aandrija10 <74418320+aandrija10@users.noreply.github.com>
2021-09-29 14:30:37 +0200

    Add files via upload

---
+++ insertSql.sql
@@ -0,0 +1,27 @@
+INSERT INTO Sampionat(Godina) 
+VALUES ('2020');
+INSERT INTO Sampionat(Godina) 
+VALUES ('2021');
+INSERT INTO Sampionat(Godina) 
+VALUES ('2022');
+
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Spa', 'Belgija', '1');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Monza', 'Italija', '1');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Monako', 'Monako', '1');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Spa', 'Belgija', '2');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Monako', 'Monako', '2');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('RB Ring', 'Austrija', '2');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Monza', 'Italija', '2');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Silverstone', 'Britanija', '1');
+INSERT INTO Trka(Naziv_Trke, Lokacija_Trke, SampionatID)
+VALUES ('Singapur', 'Singapur', '3');
+
+-- VOZACI SU DODAVANI SA KLIJENTSKE STRANE --
